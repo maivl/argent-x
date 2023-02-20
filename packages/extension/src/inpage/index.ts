fa217487a1b000f87912b9ce8c6597417d420c7c
maivl <wkf39988@gmail.com>
2023-02-20 11:32:09 +0800

    feat: inpage civia

--- packages/extension/src/inpage/index.ts
+++ packages/extension/src/inpage/index.ts
@@ -7,7 +7,7 @@ import { sendMessage, waitForMessage } from "./messageActions"
 import { getIsPreauthorized } from "./preAuthorization"
 import { starknetWindowObject, userEventHandlers } from "./starknetWindowObject"
 
-const INJECT_NAMES = ["starknet_civia"]
+const INJECT_NAMES = ["starknet", "starknet_civia"]
 
 function attach() {
   INJECT_NAMES.forEach((name) => {
